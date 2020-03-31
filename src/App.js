9dad93aac9bd9913a892ea8391b4e4eb8c382978
gyl <gylgyl2000@pm.me>
2020-03-31 19:11:34 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact
+        name="Sherri Porter"
+        avatar="https://randomuser.me/api/portraits/women/65.jpg"
+      />
+      <Contact
+        name="Patsy Herrera"
+        avatar="https://randomuser.me/api/portraits/women/26.jpg"
+        online
+      />
+      <Contact
+        name="Colleen Lee"
+        avatar="https://randomuser.me/api/portraits/women/19.jpg"
+        online
+      />
+      <Contact
+        name="Andre Hayes"
+        avatar="https://randomuser.me/api/portraits/men/69.jpg"
+      />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Contact.css";
+import PropTypes from 'prop-types';
+
+function Contact({name, avatar, online}) {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={avatar} alt={name} />
+      <div>
+        <p className="name">{name}</p>
+        <div className="status">
+          <div className={online ? "status-online" : "status-offline"} />
+          <p className="status-text">{online ? "online" : "offline"}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+};
+
+export default Contact;
